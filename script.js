013d0aeb72c09a47aa0a36670cdc95cc2262150c
SheltonT200 <155662867+SheltonT200@users.noreply.github.com>
2024-01-04 11:30:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -25,5 +25,5 @@ var b = act-(l/2);
     var b2 = b**2;
     return Math.floor((-b)+Math.sqrt(b2+(l*(j)*(2)))/(l));
 }
-i(a,20,5,c)
+i(a,20,5,b-1)
 var names = {a:'clicks',b:'cpc',c:'cps',d:'div',e:'rebirths',f:'click function',g:'buy function',h:'used in g',i:'cost function'}
