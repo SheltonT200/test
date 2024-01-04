e08d4dab0a13a4bbfd994b3cea368c30d44bf95d
SheltonT200 <155662867+SheltonT200@users.noreply.github.com>
2024-01-04 11:07:21 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+var a = 0;
+var b = 1;
+var c = 0;
+const d = document.getElementById('count');
+var e = 0;
+const f =()=>{
+  a+=b
+  d.innerText=a
+};
+const g =(h)=>{
+if (h==1){
+if (a>=cost){
+a-=cost
+b+=1
+}}else{
+if (a>=cost2){
+a-=cost2
+c+=1
+}
+}
+}
+const i = () => {
+  
+}
+var names = {a:'clicks',b:'cpc',c:'cps',d:'div',e:'rebirths',f:'click function',g:'buy function',h:'used in g',i:'cost function'}
