a7c0bcfa479d2c53f30039b64a5c2971ef6c56cc
SheltonT200 <155662867+SheltonT200@users.noreply.github.com>
2024-01-04 11:26:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,11 @@ c+=1
 }
 }
 }
-const i = () => {
-  
+const i = (j,k,l,m) => {
+var act = k+(l*m)
+var b = act-(l/2);
+    var b2 = b**2;
+    return Math.floor((-b)+Math.sqrt(b2+(l*(j)*(2)))/(l));
 }
+i(a,20,5,c)
 var names = {a:'clicks',b:'cpc',c:'cps',d:'div',e:'rebirths',f:'click function',g:'buy function',h:'used in g',i:'cost function'}
