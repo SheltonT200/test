b091acba6e181471f5b1b77a420e92fafe2ed7bc
SheltonT200 <155662867+SheltonT200@users.noreply.github.com>
2024-01-04 12:45:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,22 +8,66 @@ const f =()=>{
   d.innerText=a
 };
 const g =(h)=>{
+costs=ic(a)
+costs2=ic2(a)
 if (h==1){
-if (a>=cost){
-a-=cost
-b+=1
+r=i(a)
+if (a>=costs){
+a-=costs
+b+=1*r
 }}else{
-if (a>=cost2){
-a-=cost2
-c+=1
+r=i2(a)
+if (a>=costs2){
+a-=costs2
+c+=1*r
 }
 }
 }
-const i = (j,k,l,m) => {
+const i = (j) => {
+var k = 20
+var l = 5
+var m = b-1
 var act = k+(l*m)
-var b = act-(l/2);
-    var b2 = b**2;
-    return Math.floor((-b)+Math.sqrt(b2+(l*(j)*(2)))/(l));
+var total = (-(act-l/2) + Math.sqrt(((act-l/2)**2)+(2*l*j)))/l;
+var r = Math.floor(total)
+return r
 }
-i(a,20,5,b-1)
-var names = {a:'clicks',b:'cpc',c:'cps',d:'div',e:'rebirths',f:'click function',g:'buy function',h:'used in g',i:'cost function'}
+const ic = (j) => {
+var k = 20
+var l = 5
+var m = b-1
+var act = k+(l*m)
+var total = (-(act-l/2) + Math.sqrt(((act-l/2)**2)+(2*l*j)))/l;
+var r = Math.floor(total)
+var act2 = k+(m*l)
+var cost=(r/2)*((act2*2)+((r-1)*l))
+return cost
+}
+const i2 = (j) => {
+var k = 20
+var l = 10
+var m = c
+var act = k+(l*m)
+var total = (-(act-l/2) + Math.sqrt(((act-l/2)**2)+(2*l*j)))/l;
+var r = Math.floor(total)
+return r
+}
+const ic2 = (j) => {
+var k = 20
+var l = 10
+var m = c
+var act = k+(l*m)
+var total = (-(act-l/2) + Math.sqrt(((act-l/2)**2)+(2*l*j)))/l;
+var r = Math.floor(total)
+var act2 = k+(m*l)
+var cost=(r/2)*((act2*2)+((r-1)*l))
+return cost
+}
+var names = {a:'clicks',b:'cpc',c:'cps',d:'div',e:'rebirths',f:'click function',g:'buy function',h:'used in g',i:'cost function',jklm:'resourcesAvailable, priceStart, priceAdd, currentOwned'}
+document.addEventListener('keypress', event => {
+  if (event.code==='Digit1'){
+g(1)
+  }
+  if (event.code==='Digit2'){
+g(2)
+}})
